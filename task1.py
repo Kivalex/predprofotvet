98982670492c6a7884cb6e39941f2b99c065f547
Kivalex <122360027+Kivalex@users.noreply.github.com>
2024-03-16 12:56:35 +0300

    Update task1.py

--- task1.py
+++ task1.py
@@ -4,6 +4,12 @@ with open('monster_game.csv') as f:
     health = [sp[i][-2] for i in range(1, len(sp))]
     probability = [sp[i][2] for i in range(1, len(sp))]
     regen = []
+    '''Описание аргуменнтов
+    opportunity = значение возможности монстра
+    health = значение здоровья монстра
+    probability = значение вероятности возможности монстра
+    regen = значение регенерации монстра
+    '''
     for i in range(len(opportunity)):
         if opportunity[i] == 'СЂРµРіРµРЅРµСЂР°С†РёСЏ':
             regen.append(int(health[i]) * int(probability[i]))
